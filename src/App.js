b5f0e56bd9f45a58083c0d4cc448f76def780e4a
Dan Roberts <dan@adadevelopersacademy.org>
2018-06-11 14:20:44 -0700

    Consume pet data from an API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 import PetCollection from './components/PetCollection';
+import Status from './components/Status';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      status: {
+        message: 'loaded the page',
+        type: 'success'
+      }
+    }
+  }
+
+  updateStatus = (message, type) => {
+    this.setState({
+      status: {
+        message: message,
+        type: type
+      }
+    })
+  }
+
   render() {
 
     return (
       <div className="App">
-        <PetCollection />
+        <Status
+          message={this.state.status.message}
+          types={this.state.status.type}
+          />
+        <PetCollection
+          updateStatusCallback={this.updateStatus}
+          />
       </div>
     );
   }
--- src/components/PetCollection.js
+++ src/components/PetCollection.js
@@ -5,8 +5,11 @@ import axios from 'axios';
 import Pet from './Pet';
 import NewPetForm from './NewPetForm';
 
+const PETS_URL = 'https://petdibs.herokuapp.com/pets';
+
 class PetCollection extends Component {
   static propTypes = {
+    updateStatusCallback: PropTypes.func.isRequired
   };
 
   constructor() {
@@ -19,8 +22,31 @@ class PetCollection extends Component {
   }
 
   componentDidMount() {
-    // TODO after lunch: get some pets
-    axios.get()
+    this.props.updateStatusCallback('Loading pets...', 'success');
+    
+    axios.get(PETS_URL)
+      .then((response) => {
+        console.log('Success!');
+        console.log(response);
+
+        this.props.updateStatusCallback('Successfully loaded pets!', 'success');
+
+        // Do some pre-processing on the data
+        const pets = response.data.slice(0, 100);
+        this.setState({ pets: pets });
+      })
+      .catch((error) => {
+        console.log('Error :(');
+        console.log(error);
+
+        // Get something on the screen so the user knows
+        this.props.updateStatusCallback(error.message, 'error');
+
+        // This is what we would have done w/ jQuery.
+        // But, in React everything on the screen comes
+        // through rendering.
+        // $('.status').html(error.message)
+      });
   }
 
   addPet = (pet) => {
