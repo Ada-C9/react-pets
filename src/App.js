2958f2b636de316edf7b60b4e5f2ae2596165953
CheezItMan <chris@adadevelopersacademy.org>
2018-06-11 11:29:14 -0700

    Finished Static Code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import PetCollection from './components/PetCollection';
+
 import './App.css';
+
+const PET_LIST = [
+  {
+    name: 'Bogart',
+    age: 8,
+    breed: 'Mixed',
+    about: 'Bogart was Kirsten\'s Great cat!',
+  },
+  {
+    name: 'Atticus',
+    age: 2003,
+    breed: 'Irish Wolfhound',
+    about: 'Atticus is a somewhat senior citizen!',
+  },
+  {
+    name: 'Bagel',
+    age: 1,
+    breed: 'Beagle',
+    about: 'Bagel the Beagle!',
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* fill it in!!! */}
+        <PetCollection petList={PET_LIST} />
       </div>
     );
   }
+
+
+
+
 }
 
 export default App;
--- src/components/PetCollection.js
+++ src/components/PetCollection.js
@@ -5,6 +5,56 @@ import NewPetForm from './NewPetForm';
 
 class PetCollection extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      petList: props.petList,
+    }
+  }
+
+  renderPetList = () => {
+    console.log('Rendering Pet List');
+    const componentList = this.state.petList.map((pet, index) => {
+      return (
+        <Pet
+          key={index}
+          name={pet.name}
+          age={pet.age}
+          breed={pet.breed}
+          about={pet.about}
+        />
+      );
+    });
+
+    return componentList;
+  }
+
+  addPet = (pet) => {
+    const petList = this.state.petList;
+
+    petList.push(pet);
+    this.setState({
+      petList,
+    });
+
+  }
+
+  render() {
+    return (
+      <section>
+        {this.renderPetList()}
+        <NewPetForm addPetCallback={this.addPet} />
+      </section>
+    )
+  }
 }
+
+PetCollection.propTypes = {
+  petList: PropTypes.array.isRequired,
+}
+
+
+
 
 export default PetCollection;
